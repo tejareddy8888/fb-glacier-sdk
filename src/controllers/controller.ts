0bce5b75439363ac3b69aebd189945355a17f56b
Tomer Shoham <tshoham@fireblocks.com>
2025-07-03 16:30:37 +0300

    feat: implement ApiController

---
+++ src/controllers/controller.ts
@@ -0,0 +1,77 @@
+import { readFileSync } from "fs";
+import dotenv from "dotenv";
+import { BasePath } from "@fireblocks/ts-sdk";
+import { Request, Response } from "express";
+import { ClaimApiService } from "../services/claim.api.service";
+import { ProvetreeService } from "../services/provetree.service";
+import { FireblocksService } from "../services/fireblocks.service";
+// import { generateMessage } from "../utils/generateMessage";
+dotenv.config();
+
+export class ApiController {
+  private fireblocksService: FireblocksService;
+  private claimApiService: ClaimApiService;
+  private provetreeService: ProvetreeService;
+  constructor() {
+    const secretKeyPath = process.env.FIREBLOCKS_SECRET_KEY_PATH;
+
+    const secretKey = readFileSync(secretKeyPath || "", "utf-8");
+
+    const fireblocksConfig = {
+      apiKey: process.env.FIREBLOCKS_API_KEY || "",
+      secretKey: secretKey,
+      basePath: (process.env.BASE_PATH as BasePath) || BasePath.US,
+    };
+
+    this.fireblocksService = new FireblocksService(fireblocksConfig);
+    this.claimApiService = new ClaimApiService();
+    this.provetreeService = new ProvetreeService();
+  }
+
+  public checkAddress = async (req: Request, res: Response) => {
+    const { address, chain } = req.params;
+    try {
+      const value = await this.provetreeService.checkAddress(
+        address,
+        chain as SupportedBlockchains
+      );
+      res.status(200).json(value);
+    } catch (error) {}
+  };
+
+  public makeClaim = async (req: Request, res: Response) => {
+    const {
+      chain,
+      assetId,
+      originVaultAccountId,
+      destinationVaultAccountId,
+      amount,
+    } = req.body;
+    try {
+      const fbResoponse = await this.fireblocksService.signMessage(
+        chain,
+        assetId,
+        originVaultAccountId,
+        destinationVaultAccountId,
+        amount
+      );
+
+      if (
+        !fbResoponse ||
+        !fbResoponse.signature.fullSig ||
+        !fbResoponse.publicKey
+      ) {
+        throw new Error("...");
+      }
+
+      const claimResponse = await this.claimApiService.makeClaim(
+        originVaultAccountId,
+        amount,
+        fbResoponse.signature.fullSig,
+        destinationVaultAccountId,
+        fbResoponse.publicKey
+      );
+      res.status(200).json(claimResponse);
+    } catch (error) {}
+  };
+}
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,18 @@
 import { Router, Request, Response } from "express";
+import { ApiController } from "./controllers/controller";
 
 const router: Router = Router();
 
-router.get("/health", (_req: Request, res: Response) => {
-  console.log("alive");
-  res.status(200).send("Alive");
-});
+const apiController = new ApiController();
+
+router
+  .get("/health", (_req: Request, res: Response) => {
+    console.log("alive");
+    res.status(200).send("Alive");
+  })
+  .get("/provetree/check/:chain/:address", apiController.checkAddress)
+  .get("/claim/check/:chain/:address", apiController.checkAddress);
+
+router.post("/claim/claims/:chain/:address", apiController.makeClaim);
 
 export default router;
