754bf44ef7e63ebd701da50ea7dc0b1d33bb4fb1
Tomer Shoham <tshoham@fireblocks.com>
2025-07-03 16:32:02 +0300

    feat: implement checkAddress and getProofData methods in ProvetreeService

--- src/services/provetree.service.ts
+++ src/services/provetree.service.ts
@@ -1,3 +1,55 @@
+import axios from "axios";
+
+import { midnightProvtreeAdress } from "../constants";
 export class ProvetreeService {
-  constructor() {}
+  public checkAddress = async (
+    address: string,
+    blockchainId: SupportedBlockchains
+  ): Promise<number> => {
+    try {
+      const response = await axios.get(
+        `${midnightProvtreeAdress}/check/${blockchainId}/${address}`,
+        {
+          headers: {
+            Accept: "application/json;charset=utf-8",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        return response.data.value;
+      } else {
+        throw new Error(`Unexpected response status: ${response.status}`);
+      }
+    } catch (error: any) {
+      console.error(
+        `Error fetching data for address ${address}:`,
+        error.message
+      );
+      throw error;
+    }
+  };
+
+  public getProofData = async (
+    apiKey: string,
+    requestData: object
+  ): Promise<any> => {
+    try {
+      const response = await axios.post(midnightProvtreeAdress, requestData, {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
+
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        throw new Error(`Unexpected response status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error("Error fetching proof data:", error);
+      throw error;
+    }
+  };
 }
