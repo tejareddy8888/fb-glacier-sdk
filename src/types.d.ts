19bf40e294bdd74a7875c73707a3ff2a14340440
Tomer Shoham <tshoham@fireblocks.com>
2025-07-03 16:30:05 +0300

    feat: add types file

---
+++ src/types.d.ts
@@ -0,0 +1,64 @@
+enum SupportedBlockchains {
+  CARDANO = "cardano",
+  BITCOIN = "bitcoin",
+  ETHEREUM = "ethereum",
+  EVM = "evm",
+  SOLANA = "solana",
+  AVALANCHE = "avalanche",
+  XRP = "xrp",
+}
+
+enum SupportedAssetIds {
+  ADA = "ADA",
+  BTC = "BTC",
+  ETH = "ETH",
+  BNB = "BNB",
+  BAT = "BAT",
+  SOL = "SOL",
+  AVAX = "AVAX",
+  XRP = "XRP",
+}
+
+enum TypedMessagesTypes {
+  EIP191 = "EIP191",
+  EIP712 = "EIP712",
+  BTC = "BTC_MESSAGE",
+}
+
+interface MessagePayloadSource {
+  type: TransferPeerPathType;
+  id: string;
+}
+
+interface TypedMessages {
+  content: any;
+  type: TypedMessagesTypes;
+}
+
+interface RawMessageData {
+  messages: TypedMessages[];
+}
+
+interface MessagePayloadSourceExtraParameters {
+  rawMessageData: RawMessageData;
+}
+
+interface MessagePayload {
+  operation?: TransactionOperation;
+  assetId: SupportedBlockchains;
+  source?: MessagePayloadSource;
+  note?: string;
+  vaultAccountId?: string;
+  bip44addressIndex?: number;
+  message?: string;
+  extraParameters?: MessagePayloadSourceExtraParameters;
+}
+
+interface RawSigningConfig {
+  assetId: string;
+  message: string;
+  vaultAccountId: string;
+  bip44addressIndex?: number;
+  note?: string;
+  extraParameters?: Record<string, any>;
+}
