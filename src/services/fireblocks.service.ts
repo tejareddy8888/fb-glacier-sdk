256d1dcd41f0291c690cbc9ddc5ad644b961dbbf
Tomer Shoham <tshoham@fireblocks.com>
2025-07-03 16:31:34 +0300

    feat: enhance FireblocksService with signMessage method and constructor configuration

--- src/services/fireblocks.service.ts
+++ src/services/fireblocks.service.ts
@@ -1,5 +1,65 @@
-import { Fireblocks } from "@fireblocks/ts-sdk";
+import {
+  ConfigurationOptions as FireblocksConfig,
+  Fireblocks,
+  SignedMessageSignature,
+} from "@fireblocks/ts-sdk";
+import {
+  generateTransactionPayload,
+  rawSignRawMessage,
+} from "../utils/fireblocks.utils";
+import { getVaultAccountAddress } from "../utils/general";
 
 export class FireblocksService {
   private readonly fireblocksSDK: Fireblocks;
+
+  constructor(config: FireblocksConfig) {
+    this.fireblocksSDK = new Fireblocks({
+      apiKey: config.apiKey,
+      secretKey: config.secretKey,
+      basePath: config.basePath,
+    });
+  }
+
+  public signMessage = async (
+    chain: SupportedBlockchains,
+    assetId: SupportedAssetIds,
+    originVaultAccountId: string,
+    destinationVaultAccountId: string,
+    amount: number
+  ): Promise<{
+    signature: SignedMessageSignature;
+    publicKey?: string;
+    content?: string;
+  } | null> => {
+    try {
+      const originAddress = await getVaultAccountAddress(
+        this.fireblocksSDK,
+        originVaultAccountId,
+        assetId
+      );
+      const destinationAddress = await getVaultAccountAddress(
+        this.fireblocksSDK,
+        destinationVaultAccountId,
+        assetId
+      );
+      const transactionPayload = generateTransactionPayload(
+        chain,
+        assetId,
+        originAddress,
+        destinationAddress,
+        amount
+      );
+      if (!transactionPayload) {
+        throw new Error("...");
+      }
+      const response = await rawSignRawMessage(
+        this.fireblocksSDK,
+        transactionPayload
+      );
+      return response;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 }
