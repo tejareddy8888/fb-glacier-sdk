41b3a03ed27919d14c560c90f367bb06253eed64
Tomer Shoham <tshoham@fireblocks.com>
2025-07-03 16:31:51 +0300

    feat: implement claimByAddress and makeClaim methods in ClaimApiService

--- src/services/claim.api.service.ts
+++ src/services/claim.api.service.ts
@@ -1,3 +1,47 @@
+import axios from "axios";
+import { midnightClainAdress } from "../constants";
+
 export class ClaimApiService {
   constructor() {}
+  public claimByAddress = async (
+    address: string,
+    blockchainId: SupportedBlockchains
+  ) => {
+    try {
+      let params = {
+        // address: address,
+        // amount: provtree_response_amount,
+        // cose_sign1: message.signature,
+        // dest_address: destination_address,
+        // cose_key: message.key,
+      };
+      const response = await axios.get(
+        `${midnightClainAdress}/claims/${blockchainId}?address=${address}`,
+        {
+          headers: {
+            Accept: "application/json;charset=utf-8",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        throw new Error(`Unexpected response status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(`Error fetching claims for address ${address}:`, error);
+      throw error;
+    }
+  };
+  public makeClaim = async (
+    originVaultAccountId: string,
+    amount: number,
+    fullSig: string,
+    destinationAddress: string,
+    publicKey: string
+  ): Promise<any> => {
+    try {
+    } catch (error) {}
+  };
 }
