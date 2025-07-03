ab8611eb13e25fd84a5aafc1f69a5cbb4ba1fbae
Tomer Shoham <tshoham@fireblocks.com>
2025-07-03 16:29:46 +0300

    feat: add constants file

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const midnightProvtreeAdress =
+  "https://proof-staging.provtree-midnight.com";
+
+export const midnightClainAdress = "https://external-claim.gd.midnighttge.io";
+
+export const termsAndConditionsHash =
+  "6bf2adf825baa496729e2eac1e895ebc77973744bce67f44276bf6006f5c21de863ed121e11828d8fc0241773191e26dc1134803a681a9a98ba0ae812553db24";
