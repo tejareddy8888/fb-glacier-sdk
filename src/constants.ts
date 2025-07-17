4e119a8449ecb5591f91d856fc68cab5bb663e58
Tomer Shoham <tshoham@fireblocks.com>
2025-07-17 11:02:24 +0300

    feat: nightToken + tx fee

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,7 @@ export const midnightClaimAdress = "https://external-claim.gd.midnighttge.io";
 
 export const termsAndConditionsHash =
   "6bf2adf825baa496729e2eac1e895ebc77973744bce67f44276bf6006f5c21de863ed121e11828d8fc0241773191e26dc1134803a681a9a98ba0ae812553db24";
+
+export const nightTokenName = "NIGHT";
+
+export const tokenTransactionFee = 200000;
